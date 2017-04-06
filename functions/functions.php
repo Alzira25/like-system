8daed26a531a7e3c50fc71c1f79fe79b6e547d71
Alzira25 <alzirabarretto@gmail.com>
2017-04-06 12:40:11 +0530

    Create functions.php

---
+++ functions/functions.php
@@ -0,0 +1,13 @@
+//getting the user IP address
+function getIp() {
+    $ip = $_SERVER['REMOTE_ADDR'];
+ 
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+ 	$ip=$_SERVER['REMOTE_ADDR'];
+ }
+    return $ip;
+}
